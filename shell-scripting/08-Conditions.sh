64abd270165c4651ff6255706d7fe57cc1af24e6
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 16:58:19 +0530

    conditions

--- shell-scripting/08-Conditions.sh
+++ shell-scripting/08-Conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-number=$1     #if_[_......]
+number=$1     #if_[_......_]
               #then
                  #......
               #else
                 #....
               #fi
 
-if [ $number -gt 100]
+if [ $number -gt 100 ]
 then
     echo "given number $number is greaerthen."
 else
