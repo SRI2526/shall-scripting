4ed94ab9f6310016d72827cf598fa2092b54073a
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 09:04:24 +0530

    new chinges

--- shell-scripting/02-variables.sh
+++ shell-scripting/02-variables.sh
@@ -1,5 +1,6 @@
-pursone1="prasanna kumar reddy"
+pursone1="prasanna kumar reddy" #verabls is using capital OR small.
 pursone2="sri rama krishna reddy"
 
 echo "$pursone1 ; Hello $pursone2, Good Morning?"
-echo "$pursone2 ;Hello $pursone1, Very Good Morning."
+echo "$pursone2 ; Hello $pursone1, Very Good Morning."
+echo "$pursone1 ; ఎలవునవు $pursone2 అందరూ బాగుణరా?"
