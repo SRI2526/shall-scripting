aef33693b00ee5b9eec2b6bc0fd154a205169192
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 21:28:55 +0530

    commitment

--- shell-scripting/09-installin-packeges.sh
+++ shell-scripting/09-installin-packeges.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 ID=$(id -u) # ROOT లో వుంటే 0 వస్తుంది.ROOR లో లేకపోతే NUMNER వస్తుంది./ROOT(SUDO SU)USER అవునా కాదా అని ID లో STORE అవుతుంది. 
 
-RED="\e[31m"
-N="\e[0m"
-g="\e[32m"
 if [ $ID -ne 0 ]
 then
-    echo -e "$RED ERROR $N:: SORRY YOU ARE NOT ROOR USER,PLISE TAKE ROOT USER ACCESS."
-    exit 1
+    echo -e "ERROR :: SORRY YOU ARE NOT ROOR USER,PLISE TAKE ROOT USER ACCESS."
+    exit 1 #ఇది error వస్తే ఇకడితో stop అవుతుంది.
 
 else
     echo "YOU ARE IN THA ROOT USER."
 fi
 #----------------------------------------------------------------------------------------------------------
 yum install mysql -y #packege install చేయడం.
-echo -e "installstion $g SUCCESS $N"
+#----------------------------------------------------------------------------------------------------------
+if [ $? -ne 0 ] # $? అనేది cummand success అవతే  0 వస్తుంది. లేదా number వస్తుంది.
+then
+    echo "ERROR :: Instaling MySql is filed."
+    exit 1
+else
+    echo "MySql Instaling id success."
+fi
+#-----------------------------------------------------------------------------------------------------------
