26c821ada6274c2937d8b7296ea7000d50814f6d
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 16:54:52 +0530

    conditions

---
+++ shell-scripting/07-Conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+number=$1     #if_[_......]
+              #then
+                 #......
+              #else
+                #....
+              #fi
+
+if [ $number -gt 100]
+then
+    echo "given number $number is greaerthen."
+else
+    echo "given number $number is not greaerthen."
+fi
