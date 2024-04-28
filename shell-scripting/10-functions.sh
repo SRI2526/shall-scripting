cd2644d41af35164e4fdf5e79ccbd0e9f708f5a4
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 21:47:16 +0530

    function

---
+++ shell-scripting/10-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ID=$(id -u) # ROOT లో వుంటే 0 వస్తుంది.ROOR లో లేకపోతే NUMNER వస్తుంది./ROOT(SUDO SU)USER అవునా కాదా అని ID లో STORE అవుతుంది. 
+#------------------------------------------------------------------------------------------------------
+functions(){ # function repeed the code 
+    if [ $? -ne 0 ] # $? అనేది cummand success అవతే  0 వస్తుంది. లేదా number వస్తుంది.
+    then
+        echo "ERROR :: Instaling java is filed."
+        exit 1
+    else
+        echo "java Instaling id success."
+    fi
+}
+#------------------------------------------------------------------------------------------------------
+if [ $ID -ne 0 ]
+then
+    echo -e "ERROR :: SORRY YOU ARE NOT ROOR USER,PLISE TAKE ROOT USER ACCESS."
+    exit 1 #ఇది error వస్తే ఇకడితో stop అవుతుంది.
+
+else
+    echo "YOU ARE IN THA ROOT USER."
+fi
+#----------------------------------------------------------------------------------------------------------
+yum install java -y #packege install చేయడం.
+#----------------------------------------------------------------------------------------------------------
+functions #function ని call చేయడం. 
+#----------------------------------------------------------------------------------------------------------
