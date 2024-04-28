732e375e68fe59e7932c95000c4bfcefc9b66567
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 11:41:10 +0530

    username-password

--- shell-scripting/05-username-password.sh
+++ shell-scripting/05-username-password.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-username=$1
-password=$2
+#username=$1
+#password=$2
+echo "Plise enter username::"
+read -s username
+echo "plise enter password::"
+read -s password
 
 echo "username is = $username , password is = $password"
