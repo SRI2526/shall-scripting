8601a5a134c9235e29697d8164f3c8675dc56613
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 09:34:04 +0530

    a

--- shell-scripting/02-variables.sh
+++ shell-scripting/02-variables.sh
@@ -2,4 +2,4 @@ pursone1="prasanna kumar reddy" #verabls is using capital OR small.
 pursone2="sri rama krishna reddy"
 
 echo "$pursone1 ; Hello $pursone2, Good Morning?"
-echo "$pursone2 ; Hello $pursone1, Very Good Morning.."
+echo "$pursone2 ; Hello $pursone1, Very Good Morning."
