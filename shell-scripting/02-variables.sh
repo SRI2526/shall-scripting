04c771fba58da853a4a728f72db9b36e5c426399
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 09:39:14 +0530

    chinge

--- shell-scripting/02-variables.sh
+++ shell-scripting/02-variables.sh
@@ -3,3 +3,5 @@ pursone2="sri rama krishna reddy"
 
 echo "$pursone1 ; Hello $pursone2, Good Morning?"
 echo "$pursone2 ; Hello $pursone1, Very Good Morning."
+echo "$pursone1 ; how are you $pursone2."
+echo "$pursone2 ; im good $pursone1."
