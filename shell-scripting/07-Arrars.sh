cd66aa0f47d597ad7653cda42e43e712dfb49c1e
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 12:15:50 +0530

    Arriary

--- shell-scripting/07-Arrars.sh
+++ shell-scripting/07-Arrars.sh
@@ -5,4 +5,4 @@ echo "1st fruit name =${FRUITS[0]}"
 echo "2st fruit name =${FRUITS[1]}"
 echo "3st fruit name =${FRUITS[2]}"
 
-echi "All fruit names = ${FRUITS[@]}" # "@" అన్నీ రకాలు వస్తాయి అని అర్ధం.  
+echo "All fruit names = ${FRUITS[@]}" # "@" అన్నీ రకాలు వస్తాయి అని అర్ధం.  
