d4c93c62857b2be80ca3e50cfdc9153b7a0d6135
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 09:09:47 +0530

    auther chinge

--- shell-scripting/02-variables.sh
+++ shell-scripting/02-variables.sh
@@ -3,4 +3,3 @@ pursone2="sri rama krishna reddy"
 
 echo "$pursone1 ; Hello $pursone2, Good Morning?"
 echo "$pursone2 ; Hello $pursone1, Very Good Morning."
-echo "$pursone1 ; ఎలవునవు $pursone2 అందరూ బాగుణరా?"
