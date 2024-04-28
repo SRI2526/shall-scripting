6f2485f52345e014fdef9048fa1139604d2c827a
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 11:47:06 +0530

    username-password

--- shell-scripting/05-username-password.sh
+++ shell-scripting/05-username-password.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #username=$1
 #password=$2
-echo "Plise enter username::"
+echo "Plise enter username::" #read అనేది username valu ని autometic గా $username లో lode చేస్తుంది. 
+                              # -s మనం type చేసిన దానిని run time లో hide చేస్తుంది.
 read -s username
 echo "plise enter password::"
 read -s password
