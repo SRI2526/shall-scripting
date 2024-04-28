8287650ae1c498667afae587b77a4c2b685912b0
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 18:15:30 +0530

    gfd

--- shell-scripting/09-installin-packeges.sh
+++ shell-scripting/09-installin-packeges.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ID=$(id -u) # ROOT లో వుంటే 0 వస్తుంది.ROOR లో లేకపోతే NUMNER వస్తుంది./ROOT(SUDO SU)USER అవునా కాదా అని ID లో STORE అవుతుంది. 
 
-RED= "\e[31m"
-N= "\e[0m"
+RED="\e[31m"
+N="\e[0m"
 if [ $ID -ne 0 ]
 then
     echo -e "$RED ERROR $N:: SORRY YOU ARE NOT ROOR USER,PLISE TAKE ROOT USER ACCESS."
+    
 else
     echo "YOU ARE IN THA ROOT USER."
 fi
