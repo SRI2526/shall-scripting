37052db5e7f32f4f9754a688833bfb822cff4fa3
SRI2526 <ksrkreddy.212950@gmail.com>
2024-04-28 11:25:27 +0530

    arguments

--- shell-scripting/04-arguments.sh
+++ shell-scripting/04-arguments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pursone1=$1
+pursone1=$1 #$1 ఇది argument. a , b . c ఎలా ఇవ్వకూడదు. ex : linex లో sh 04-arguments.sh prasanna krishna ఇలా ఇవ్వాలి.  
 pursone2=$2
 
 echo "$pursone1 ; Hello $pursone2, Good Morning?"
