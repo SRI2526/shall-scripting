16536b80202570ed6259e89a35f9fc3d2ced7ffc
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-01 10:22:00 +0530

    LOPES

--- shell-scripting/12-lops.sh
+++ shell-scripting/12-lops.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-for B in {1..1000} # a స్థానం లో abcd.. లు ఏమయినా ఇవ్వచ్చు. numbers కుదరదు. 
+for B in {1..1000} # a స్థానం లో abcd & ABCD.. లు ఏమయినా ఇవ్వచ్చు. numbers కుదరదు. 
 do
     echo "$B"
 done
