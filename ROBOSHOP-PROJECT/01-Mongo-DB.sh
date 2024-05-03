796ed03cb2b9349dbff90911c6dd933616e39932
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-03 12:53:05 +0530

    mongodb

--- ROBOSHOP-PROJECT/01-Mongo-DB.sh
+++ ROBOSHOP-PROJECT/01-Mongo-DB.sh
@@ -33,7 +33,7 @@ VALIDATE $? "Enable Mongo.DB"
 systemctl start mongod &>> $logfiles
 VALIDATE $? "Starting Mongo.DB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g /etc/mongod.conf' &>> $logfiles
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $logfiles
 VALIDATE $? "Esiting Remote Access to Mongo.DB"
 systemctl restart mongod
 VALIDATE $? "Restarting Mongo.DB"
