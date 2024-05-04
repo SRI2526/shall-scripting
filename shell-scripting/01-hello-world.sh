963c339a3598df4cd178a1aac49ae75ba4f4e464
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-04 21:33:55 +0530

    a

--- shell-scripting/01-hello-world.sh
+++ shell-scripting/01-hello-world.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+read filename
 name="/home/ec2-user/krishna/ram.text"
 if [ -e $name ]
 then
     echo "ram.text file is have"
+    a="0"
 else
     echo "this is no match file name"
 fi
