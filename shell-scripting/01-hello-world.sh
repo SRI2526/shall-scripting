bfbac2409c8524290b79600b4412f996ddf8017e
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-04 22:17:11 +0530

    aaa

--- shell-scripting/01-hello-world.sh
+++ shell-scripting/01-hello-world.sh
@@ -7,11 +7,7 @@ read destnastion_parth
 #name="/home/ec2-user/krishna/ram.text"
 if [ -e $file_name ]
 then
-    echo "ram.text file is have"
-    a="0"
+    echo "$?"
 else
-    echo "this is no match file name"
-    echo "File Name"
-    read file_name
+    echo "$?"
 fi
-echo "$a"
