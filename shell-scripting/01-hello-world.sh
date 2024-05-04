cb286d3f856aa310fb3cb8a1a86ea2678c467be5
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-04 22:29:36 +0530

    aaa

--- shell-scripting/01-hello-world.sh
+++ shell-scripting/01-hello-world.sh
@@ -5,9 +5,18 @@ read file_name
 echo "destnastion parth"
 read destnastion_parth
 #name="/home/ec2-user/krishna/ram.text"
+function(){
+    if [ $1 -ne 0 ]
+    then
+        echo "file is not have.plice enter File Name"
+        read file_name
+        echo "destnastion parth"
+        read destnastion_parth
+    fi
+}
 if [ -e $file_name ]
 then
-    echo "$?"
+    
 else
-    echo "$?"
+    function $?
 fi
