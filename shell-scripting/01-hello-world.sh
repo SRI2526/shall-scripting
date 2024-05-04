86ba98b49146ce52f3c7e7019769d74ff00884f4
SRI2526 <ksrkreddy.212950@gmail.com>
2024-05-04 21:55:18 +0530

    aaaa

--- shell-scripting/01-hello-world.sh
+++ shell-scripting/01-hello-world.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "File Name"
-read 
+read fff
 echo "destnastion parth"
-read 
+read ggg
 name="/home/ec2-user/krishna/ram.text"
 if [ -e $name ]
 then
@@ -12,3 +12,5 @@ else
     echo "this is no match file name"
 fi
 echo "$a"
+echo $fff
+echo $ggg
